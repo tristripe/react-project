4d910c0994725c5e39330af829a8bd3ec95449f0
tristripe <lrolfg@gmail.com>
2023-07-12 09:36:05 +0300

    refactoring by fsd

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
-import React, {Suspense, useContext, useState} from 'react';
+import React, {Suspense} from 'react';
 import {Routes, Route, Link} from 'react-router-dom';
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {AboutPage} from "pages/AboutPage";
+import {MainPageAsync} from "pages/MainPage/MainPage.async";
+import {useTheme} from "app/providers/ThemeProvider";
+import {classNames} from "helpers/classNames/classNames";
 import "./styles/index.scss";
-import {Theme, ThemeContext} from "./theme/ThemeContext";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
 
 
 const App = () => {
@@ -19,7 +18,7 @@ const App = () => {
       <Link to="/about">О сайте</Link>
       <Suspense fallback={<div>Загрузка</div>}>
         <Routes>
-          <Route path={'/about'} element={<AboutPageAsync/>}/>
+          <Route path={'/about'} element={<AboutPage/>}/>
           <Route path={'/'} element={<MainPageAsync/>}/>
         </Routes>
       </Suspense>
